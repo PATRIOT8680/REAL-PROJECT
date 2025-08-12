8cc3d857a9dc2195e3749c8c5a77516d4a9ce597
PATRIOT <147982576+PATRIOT8680@users.noreply.github.com>
2025-08-12 22:23:00 +0400

    [dev] 0.0.2.1

--- src/client/player/nametags.ts
+++ src/client/player/nametags.ts
@@ -1,57 +1,19 @@
-interface ICacheTag {
-  text: string,
-  color: Array4d
-}
+let visibleTag = true
 
-export let showPlayerNameTag = true
-
-mp.nametags.enabled = false
 mp.keys.bind(global.Keys.VK_F9, false, () => {
-  showPlayerNameTag = !showPlayerNameTag
+  visibleTag = !visibleTag
 })
 
+mp.events.add('render', (nametags) => {
+  for (nametag of nametags) {
+    const [player, x, y, distance] = nametag
+    if (distance > 50) continue
 
-
-mp.events.add('render', () => {
-  mp.peds.forEachInStreamRange((ped: PedMp) => {
-    const pedPos = ped.getBoneCoords(12844, 0, 0, 0)
-    const localPos = mp.players.local.position
-
-    const distance = mp.game.gameplay.getDistanceBetweenCoords(
-        localPos.x, localPos.y, localPos.z,
-        pedPos.x, pedPos.y, pedPos.z,
-        true
-    )
-
-    if (distance < 15) return
-    const { x, y } = mp.game.graphics.world3dToScreen2d(new mp.Vector3(pedPos.x, pedPos.y, pedPos.z + 0.5))
-
-    if (x && y) {
-      drawTag(x, y, `Ped «${ped.remoteId}»`, [255, 255, 255, 255])
-    }
-  })
-  mp.players.forEachInStreamRange((player: PlayerMp) => {
-    if (player.handle === 0 || player === mp.players.local) return
-
-    const targetPos = player.getBoneCoords(12844, 0, 0, 0)
-    const localPos = mp.players.local.position
-
-    const distance = mp.game.gameplay.getDistanceBetweenCoords(
-      localPos.x, localPos.y, localPos.z,
-      targetPos.x, targetPos.y, targetPos.z,
-      true
-    )
-
-    if (distance < 15) return
-    const { x, y } = mp.game.graphics.world3dToScreen2d(new mp.Vector3(targetPos.x, targetPos.y, targetPos.z + 0.5))
-
-    if (x && y) {
-      drawTag(x, y, `Местный «${player.remoteId}»`, [255, 255, 255, 255])
-    }
-  })
+    mp.game.graphics.drawText(player.remoteId, [x, y], {
+      font: 0,
+      color: [255, 255, 255, 255],
+      scale: [0.4, 0.4],
+      outline: true,
+    })
+  }
 })
-
-
-const drawTag = (x: number, y: number, tag: string, color: Array4d) => {
-  mp.game.graphics.drawText(tag, [x, y], { font: 2, color: color, scale: [0.3, 0.3], outline: true })
-}
