8581d50db6b9a224c31bb315b2d6b2d5bc4cbed5
RealProject86 <realrp-gta5@outlook.com>
2025-08-12 17:18:38 +0300

    [dev] 0.0.2

--- src/server/whitelist/index.ts
+++ src/server/whitelist/index.ts
@@ -1,6 +1,6 @@
 mp.events.add('playerJoin', (player) => {
   const socialClub = player.socialClub
-  const whitelist = [ 'HaseNRP', 'saint_price' ]
+  const whitelist = [ 'HaseNRP', 'Anaken74', 'whysh1n3' ]
 
   if (!whitelist.includes(socialClub)) {
     player.kick('Вы не добавлены в Whitelist!')
