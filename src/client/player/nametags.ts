8b62e169e52f3b72d349546d23d54150adda3fa2
PATRIOT <147982576+PATRIOT8680@users.noreply.github.com>
2025-08-13 00:16:35 +0400

    [dev] 0.0.2.1

--- src/client/player/nametags.ts
+++ src/client/player/nametags.ts
@@ -1,19 +1,59 @@
-let visibleTag = true
+const maxDistance = 25*25;
+const width = 0.03;
+const height = 0.0065;
+const border = 0.001;
+const color = [255,255,255,255];
 
-mp.keys.bind(global.Keys.VK_F9, false, () => {
-  visibleTag = !visibleTag
-})
+mp.nametags.enabled = false;
 
 mp.events.add('render', (nametags) => {
-  for (nametag of nametags) {
-    const [player, x, y, distance] = nametag
-    if (distance > 50) continue
+  const graphics = mp.game.graphics;
+  const screenRes = graphics.getScreenResolution();
 
-    mp.game.graphics.drawText(player.remoteId, [x, y], {
-      font: 0,
-      color: [255, 255, 255, 255],
-      scale: [0.4, 0.4],
-      outline: true,
-    })
-  }
+  nametags.forEach(nametag => {
+    let [player, x, y, distance] = nametag;
+
+    if(distance <= maxDistance) {
+      let scale = (distance / maxDistance);
+      if(scale < 0.6) scale = 0.6;
+
+      var health = player.getHealth();
+      health = health < 100 ? 0 : ((health - 100) / 100);
+
+      var armour = player.getArmour() / 100;
+
+      y -= scale * (0.005 * (screenRes.y / 1080));
+
+      mp.game.graphics.drawText(player.name.replace('_', ' '), [x, y],
+          {
+            font: 4,
+            color: [255, 255, 255, 255],
+            scale: [0.4, 0.4],
+            outline: true
+          });
+
+      if(mp.game.player.isFreeAimingAtEntity(player.handle)) {
+        let y2 = y + 0.042;
+
+        if(armour > 0) {
+          let x2 = x - width / 2 - border / 2;
+
+          graphics.drawRect(x2, y2, width + border * 2, 0.0085, 0, 0, 0, 200, false);
+          graphics.drawRect(x2, y2, width, height, 150, 150, 150, 255, false);
+          graphics.drawRect(x2 - width / 2 * (1 - health), y2, width * health, height, 255, 255, 255, 200, false);
+
+          x2 = x + width / 2 + border / 2;
+
+          graphics.drawRect(x2, y2, width + border * 2, height + border * 2, 0, 0, 0, 200, false);
+          graphics.drawRect(x2, y2, width, height, 41, 66, 78, 255, false);
+          graphics.drawRect(x2 - width / 2 * (1 - armour), y2, width * armour, height, 48, 108, 135, 200, false);
+        }
+        else {
+          graphics.drawRect(x, y2, width + border * 2, height + border * 2, 0, 0, 0, 200, false);
+          graphics.drawRect(x, y2, width, height, 150, 150, 150, 255, false);
+          graphics.drawRect(x - width / 2 * (1 - health), y2, width * health, height, 255, 255, 255, 200, false);
+        }
+      }
+    }
+  })
 })
