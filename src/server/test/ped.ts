638c41d60c9803afa67a6c2d770a2634ef57036f
PATRIOT <147982576+PATRIOT8680@users.noreply.github.com>
2025-08-12 18:58:46 +0400

    [dev] 0.0.2.1

---
+++ src/server/test/ped.ts
@@ -0,0 +1,7 @@
+const pedPos = new mp.Vector3(1948.4307861328125, 3916.800048828125, 38.833740234375)
+
+mp.peds.new(mp.joaat('mp_m_freemode_01'), pedPos, {
+  dynamic: true,
+  frozen: false,
+  invincible: true
+})
