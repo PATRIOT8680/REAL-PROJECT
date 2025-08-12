006f56ffcad60771428d12c5c4b5dc3fba1655a3
PATRIOT <147982576+PATRIOT8680@users.noreply.github.com>
2025-08-12 18:38:12 +0400

    [dev] 0.0.2.1

--- src/client/player/nametags.ts
+++ src/client/player/nametags.ts
@@ -4,64 +4,35 @@ interface ICacheTag {
 }
 
 export let showPlayerNameTag = true
-let cacheTags: ICacheTag[] = []
-const maxDistance = 25*25;
 
 mp.nametags.enabled = false
 mp.keys.bind(global.Keys.VK_F9, false, () => {
   showPlayerNameTag = !showPlayerNameTag
 })
 
-const calcDistance = (v1, v2) =>  {
-  const dx = v1.x - v2.x;
-  const dy = v1.y - v2.y;
-  const dz = v1.z - v2.z;
 
-  return Math.sqrt(dx * dx + dy * dy + dz * dz);
-}
 
-mp.events.add('render', (nametags) => {
-  try {
-    if (global.loginPlayer) return
-    mp.console.logWarning('1')
+mp.events.add('render', () => {
+  mp.players.forEachInStreamRange((player: PlayerMp) => {
+    const targetPos = player.getBoneCoords(12844, 0, 0, 0)
+    const localPos = mp.players.local.position
 
-    const playerPos = mp.players.local.position
-    const target = mp.players.local
+    const distance = mp.game.gameplay.getDistanceBetweenCoords(
+      localPos.x, localPos.y, localPos.z,
+      targetPos.x, targetPos.y, targetPos.z,
+      true
+    )
 
-    if (showPlayerNameTag) {
-      mp.console.logWarning('2')
-      nametags.forEach(nametag => {
-        let [player, x, y, distance] = nametag
-        mp.console.logWarning('2.1')
+    if (distance < 15) return
+    const { x, y } = mp.game.graphics.world3dToScreen2d(new mp.Vector3(targetPos.x, targetPos.y, targetPos.z + 0.5))
 
-        if (calcDistance(playerPos, player.position) < 15) {
-          mp.console.logWarning('3')
-          if (player.vehicle) y += 0.5
-
-          if (cacheTags[player.remoteId] === undefined) {
-            mp.console.logWarning('4')
-            let text: string = `Местный «${player.remoteId}»`
-            let color: Array4d = [255, 255, 255, 255]
-
-            cacheTags[player.remoteId] = { text, color }
-          }
-
-          const label: ICacheTag = cacheTags[player.remoteId]
-          if (label) {
-            mp.console.logWarning('5')
-            mp.console.logWarning(`${player.remoteId} ${x} ${y} ${label.text} ${label.color}`)
-            drawTag(player, x, y, label.text, label.color)
-          }
-        }
-      })
+    if (x && y) {
+      drawTag(x, y, `Местный «${player.remoteId}»`, [255, 255, 255, 255])
     }
-  } catch (e) {
-
-  }
+  })
 })
 
 
-const drawTag = (player: PlayerMp, x: number, y: number, tag: string, color: Array4d) => {
-  mp.console.logWarning('11')
+const drawTag = (x: number, y: number, tag: string, color: Array4d) => {
   mp.game.graphics.drawText(tag, [x, y], { font: 2, color: color, scale: [0.3, 0.3], outline: true })
 }
