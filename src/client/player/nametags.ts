43c208eda8b9f7476da39f60490f0ccde2330ef2
PATRIOT <147982576+PATRIOT8680@users.noreply.github.com>
2025-08-15 21:22:51 +0400

    [dev] 0.0.2.8

--- src/client/player/nametags.ts
+++ src/client/player/nametags.ts
@@ -53,11 +53,9 @@ const drawNametags = (player: PlayerMp, x: number, y: number, displayName: strin
   )
 
   if (player.getVariable('player_knockout')) {
-    const scale = Math.max(0.1, 1 - distance / 25)
-    const scaleSprite = 0.7 * scale
-    drawSprite('commonmenutu', 'team_deathmatch', [x - 0.05, textY - 0.05], [scaleSprite, scaleSprite], 0, [255, 13, 74, 255])
+    drawSprite('commonmenutu', 'team_deathmatch', [x - 0.05, textY - 0.05 ], [0.3, 0.3], 0, [255, 13, 74, 255])
 
-    mp.game.graphics.drawText(`Без сознания... (${deathInSeconds} sec.)`, [x, textY + 0.1],
+    mp.game.graphics.drawText(`Без сознания... (${deathInSeconds} sec.)`, [x, textY - 0.05],
       {
         font: 0,
         color: [255, 13, 74, 255],
