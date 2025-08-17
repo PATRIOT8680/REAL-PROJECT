44825f4c57a793362eab1f87945c7fd3499754af
RealProject86 <realrp-gta5@outlook.com>
2025-08-18 01:47:52 +0300

    [dev] 0.0.3.1

--- src/server/player/voice.ts
+++ src/server/player/voice.ts
@@ -1,9 +1,7 @@
-import { rpc } from '../utils/rpc'
-
-rpc.register('client:voice:new', (player: PlayerMp, target: PlayerMp) => {
+mp.events.add('client:voice:new', (player: PlayerMp, target: PlayerMp) => {
   if (target) player.enableVoiceTo(target)
 })
 
-rpc.register('client:voice:deleted', (player: PlayerMp, target: PlayerMp) => {
+mp.events.add('client:voice:deleted', (player: PlayerMp, target: PlayerMp) => {
   if (target) player.disableVoiceTo(target)
 })
