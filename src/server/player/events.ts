ebe76e3134f7a793f6fb6a31c7eb22440b7f2ac5
PATRIOT <147982576+PATRIOT8680@users.noreply.github.com>
2025-08-28 21:32:10 +0400

    [dev] 0.0.4

--- src/server/player/events.ts
+++ src/server/player/events.ts
@@ -7,11 +7,3 @@ rce.registerClientCef('getIdPlayer', (player: PlayerMp) => {
 rce.registerClientCef('player:mute', (player: PlayerMp, state: boolean) => {
   player.setVariable('player_mute', state)
 })
-
-mp.events.add('playerJoin', (player: PlayerMp) => {
-  player.setVariable('player_online', true)
-})
-
-mp.events.add('playerQuit', (player: PlayerMp) => {
-  player.setVariable('player_online', false)
-})
