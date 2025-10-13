d28b38fc427647df96f957558c51df08aae78802
unknown <kolomoez86@yandex.ru>
2025-10-13 05:40:29 +0400

    [dev] 0.0.4.5

--- src/cef/src/views/AdminMenu/pages/Logs.tsx
+++ src/cef/src/views/AdminMenu/pages/Logs.tsx
@@ -1,9 +1,9 @@
 import './assets/styles/compiled-css/Logs.css'
 import { useState } from "react";
 
-import HeaderLogs from '../Logs/components/Header.tsx'
-import EconomicsContainer from '../Logs/pages/Economics.tsx'
-import ReportsContainer from '../Logs/pages/Reports.tsx'
+import HeaderLogs from "./Logs/components/Header.tsx";
+import EconomicsContainer from "./Logs/pages/Economics.tsx";
+import ReportsContainer from "./Logs/pages/Reports.tsx";
 
 const LogsPage = () => {
   const [activeContainer, setActiveContainer] = useState<string>('economics');
